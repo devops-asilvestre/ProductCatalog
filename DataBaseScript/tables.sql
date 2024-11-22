55ad47b3c6ba357f5fba358058e480c9addf4297
Alessandro Silvestre <devops.asilvestre@gmail.com>
2024-11-22 14:12:32 -0300

    add script create table

---
+++ DataBaseScript/tables.sql
@@ -0,0 +1,17 @@
+﻿USE [ProductCatalogDb]
+GO
+/****** Object:  Table [dbo].[Products]    Script Date: 22/11/2024 14:11:19 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[Id] [int] NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Price] [decimal](18, 0) NOT NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
